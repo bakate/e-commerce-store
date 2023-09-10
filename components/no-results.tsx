1b3f5c164f74d7c70705ceb76448a2c33fbf1270
Bakate <bakateba@gmail.com>
2023-09-10 15:49:15 +0200

    feat(no-result): create a NoResult component

---
+++ components/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="grid place-items-center h-full  text-gray-500">
+      No results found.
+    </div>
+  );
+};
+
+export default NoResults;
