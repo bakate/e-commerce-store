2df0f1ba601b1638209e48da623a9403433978b5
Bakate <bakateba@gmail.com>
2023-09-10 15:57:21 +0200

    feat(loader): add a loading page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,25 @@
-import Footer from "@/components/footer";
-import { ModeToggle } from "@/components/mode-toggle";
+import { getBillboardById } from "@/actions/get-billboard";
+import { getProducts } from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+import { Suspense } from "react";
+import Loading from "./loading";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboardById("64f35a96ff7189d58e026534");
+
   return (
-    <main>
-      <ModeToggle />
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to{" "}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Next.js!
-        </a>
-      </h1>
-
-      <Footer />
-    </main>
+    <Suspense fallback={<Loading />}>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Featured Products" products={products} />
+          </div>
+        </div>
+      </Container>
+    </Suspense>
   );
 }
